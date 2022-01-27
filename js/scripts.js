fb5822049db74266473455dffb950c47c56537fd
Fernando Valdez <fervaldezjr11@gmail.com>
2022-01-27 10:23:09 +0100

    added tailwind y add scripts file

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let celcius = Number(prompt("Introduce los grados celcius"));
+
+const convertion = (celcius) => {
+  return (let = farenheit = celcius * 1.8 + 32);
+};
+
+alert("Los grados en Farenheit son " + convertion(celcius));
+
+//
+
+let numero = Number(
+  prompt("Introduce un número para averiguar su tabla de múltiplicación")
+);
+
+for (i = 1; i <= 10; i++) {
+  if (numero === numero && numero !== 0) {
+    alert(numero + " x " + i + " = " + numero * i);
+  } else {
+    alert("Error! Has introducido otro tipo de dato");
+    break;
+  }
+}
