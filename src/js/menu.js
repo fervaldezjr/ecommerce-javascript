c8984506b935004ccea004fa3ed98edfd5a84b02
Fernando Valdez <fervaldezjr11@gmail.com>
2022-02-23 22:19:18 +0100

    add libraries

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,26 @@
 let menu = document.getElementById('menu');
 let nav = document.getElementById('nav-list')
+let cerrar = document.getElementById('close');
 
 menu.addEventListener('click', show);
+cerrar.addEventListener('click', show);
 
 function show() {
     nav.classList.toggle('-translate-x-[0]')
+}
+
+// Sweet Alert
+
+let tramitar = document.getElementById('tramitar-pedido');
+
+tramitar.addEventListener('click', agregado);
+
+function agregado() {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Tu compra se ha efectuado exitosamente',
+        showConfirmButton: false,
+        timer: 1500
+      })
 }
